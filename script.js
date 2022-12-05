aa2c107fdd50d30ef7836007a570a25b0f1277e9
jericjan <cantilfrederick@gmail.com>
2022-12-05 18:51:11 +0800

    new site begins

---
+++ script.js
@@ -0,0 +1,81 @@
+var names = ['Kur0','jericjan','fcantil']
+var duration = 2000
+
+for (var name of names) {	
+	var parentDiv = $("<div></div>")
+	parentDiv.attr("class",`textParent`)
+
+	for (var letter of name) {
+		console.log(letter)
+		var letterElement = $("<h1></h1>").text(letter);  
+		letterElement.css("display", "none")
+		
+		letterElement.css("opacity", "0")	
+		parentDiv.append(letterElement)
+		$("#nameDiv").append(parentDiv)
+	}
+}
+
+
+
+var namesCount = names.length
+
+function wavyText(index) {
+anime({
+  targets:`#nameDiv > div:nth-child(${index}) > h1`,
+  translateY:[0,-20,20,0],
+  opacity:[0,1,1,0],
+  duration:duration,
+  delay: anime.stagger(100),
+  easing:"easeInOutCubic",
+  begin: function(anim) {
+	  console.log("starting")
+	  anim.animatables.forEach(function(letter) {
+		$(letter.target).css("display","inline")
+	  })
+  },
+  complete: function(anim) {
+   console.log('completed : ' + anim.completed)
+	  anim.animatables.forEach(function(letter) {
+	$(letter.target).css("display","none")
+	  })
+	  if (index == namesCount){
+		wavyText(1)
+	  }else{
+		wavyText(index+1)
+	  }
+  }  
+})
+}
+wavyText(1)
+
+var epicDivisorCount = 50
+for (var i=1;i<=epicDivisorCount; i++) {
+$("#epicDivisorParent").append($("<div class='epicDivisor'></div>"))
+$("#map").append($("<div class='epicMap'></div>"))
+}
+
+// $('.epicMap').click(function(){
+	// alert("CLICKL")
+// })
+
+$('.epicMap').click(function(){
+    var index = $('.epicMap').index(this)
+	anime({
+		targets: ".epicDivisor",
+		delay: anime.stagger(20, {
+		  grid: [epicDivisorCount, 0],
+		  from: index
+		}),
+		//translateY:[0,20,-20,0],
+		scaleY:[1,30,1],
+		opacity:[0,1,0],
+		easing:"easeInOutCubic"
+	  })
+
+});
+
+
+// [1,2,3].forEach(function(num) {
+	// wavyText(num)
+// })
